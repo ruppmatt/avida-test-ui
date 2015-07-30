bf12c9ead907f8b0772148467977b009f6b212fc
Matthew R. Rupp <ruppmatt@gmail.com>
2015-07-30 14:02:52 -0400

    Initial commit with population stat and single-value data in an array of 1k updates with 500ms timer.

---
+++ ui-test-base.js
@@ -0,0 +1,26 @@
+var update = 0;
+
+var update_timer = setInterval(doUpdate, 500);
+
+function doUpdate(){
+   update = update + 1;
+}
+
+function doPopulationStats(){
+   var cur_stats = pop_stats[update];
+   cur_stats.key="PopulationStatistics";
+   postMessage(cur_stats);
+}
+
+
+onmessage = function(e) {
+   var msg = e.data;
+   switch(msg){
+      case "GetPopulationStats":
+         doGetPopulationStats();
+         break;
+   }
+}
+
+
+
